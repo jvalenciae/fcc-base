7cd29bbb897bf7d3fd2c696bfa9e2fe873ed19b2
Leonardo Sanclemente <leonardo.sanclemente@koombea.com>
2023-07-19 18:31:03 -0500

    [FCC-56] moved factories definition to the development and test envs only

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module FccSimeon2Api
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.factory_bot.definition_file_paths = ['spec/factories']
   end
 end
