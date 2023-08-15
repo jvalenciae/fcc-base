8579829b9487a71c647c61e62a437f20dfa07aa9
Javier Valencia <jvalenciae@uninorte.edu.co>
2023-08-15 12:08:02 -0500

    Add invitedBy param to reset password link (#25)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,11 @@
 class UserMailer < ApplicationMailer
   def password_reset(user)
     to = user.email
+    hostname = ENV.fetch('FRONTEND_HOSTNAME')
+    token = user.reset_password_token
     substitutions = {
       subject: I18n.t('mailer.user.password_reset'),
-      password_reset_url: "#{ENV.fetch('FRONTEND_HOSTNAME')}/restablecer-contrasena?token=#{user.reset_password_token}"
+      password_reset_url: "#{hostname}/restablecer-contrasena?token=#{token}"
     }
     template_id = 'd-f2fc5645567746519dc51b38299625fb'
     MailerService.mail(to, substitutions, template_id)
@@ -13,11 +15,15 @@ class UserMailer < ApplicationMailer
 
   def invitation(user, current_user)
     to = user.email
+    hostname = ENV.fetch('FRONTEND_HOSTNAME')
+    first_name = current_user.first_name
+    last_name = current_user.last_name
+    token = user.reset_password_token
     substitutions = {
       subject: I18n.t('mailer.user.invitation'),
-      first_name: current_user.first_name,
-      last_name: current_user.last_name,
-      password_reset_url: "#{ENV.fetch('FRONTEND_HOSTNAME')}/restablecer-contrasena?token=#{user.reset_password_token}"
+      first_name: first_name,
+      last_name: last_name,
+      password_reset_url: "#{hostname}/restablecer-contrasena?token=#{token}&invitedBy=#{first_name}+#{last_name}"
     }
     template_id = 'd-3e3e4e1be0624523b4ed178ccafd5d8e'
     MailerService.mail(to, substitutions, template_id)
